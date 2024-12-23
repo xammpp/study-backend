f2132aca3e3879327d5c4c223f2bf60314798abf
xammpp <bagusdaffaf01@gmail.com>
2024-12-23 13:22:52 +0700

    add py

--- python/sesi-3/main.py
+++ python/sesi-3/main.py
@@ -9,11 +9,13 @@ goa_kosong = [bentuk_goa] * 4
 goa = goa_kosong.copy()
 goa[ular_position - 1] = "|0_0|"
 
+goa_kosong = ' '.join(goa_kosong)
+goa = ' '.join(goa)
+
 name_user = input("Masukkan nama kamu: ")
-# Menggunakan join() untuk menampilkan goa_kosong tanpa tanda kurung
 print(f'''
 Halo {name_user}, kamu akan menebak posisi ular yang benar.
-{' '.join(goa_kosong)}
+{goa_kosong}
 ''')
 pilihan_user = int(input("Pilih posisi ular (1 / 2 / 3 / 4): "))
 
@@ -22,11 +24,10 @@ if confirm_answer == "t":
     print(f"Program dihentikan")
     exit()
 elif confirm_answer == "y":
-    # Menggunakan join() untuk menampilkan goa tanpa tanda kurung
     if pilihan_user == ular_position:
-        print(f"{' '.join(goa)} \nSelamat {name_user}, kamu berhasil menebak posisi ular.")
+        print(f"{goa_kosong} \nSelamat {name_user}, kamu berhasil menebak posisi ular.")
     else:
-        print(f"{' '.join(goa)} \nMaaf {name_user}, kamu gagal menebak posisi ular.")
+        print(f"{goa_kosong} \nMaaf {name_user}, kamu gagal menebak posisi ular.")
 else:
     print("Kamu salah memasukkan input")
     exit()
