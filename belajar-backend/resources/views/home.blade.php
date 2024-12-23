bcb76f8d7e88365a62b8ede42dee19a922591027
xammpp <bagusdaffaf01@gmail.com>
2024-12-23 10:42:49 +0700

    add and restore

--- belajar-backend/resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.main')
-@extends('layouts.navbar')
-
-@section('title', 'Home')
-
-@section('container')
-    <h1>Ini Halaman Home</h1>
-    Selamat Datang {{ $name }}, {{ $role }}
-@endsection
