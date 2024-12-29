09f44961723ab18b90534b9a27f0810ec527e82e
xammpp <bagusdaffaf01@gmail.com>
2024-12-29 18:34:36 +0700

    up

---
+++ javascript/js/main.js
@@ -0,0 +1,16 @@
+// using function
+/*
+function myFunction() {
+  document.getElementById('demo').innerHTML = 'Hello Javascript';
+  document.getElementById('demo').style.color = 'red';
+}
+*/
+
+// using event listener
+/*
+document.getElementById('demo1').style.fontSize = '30px';
+document.getElementById('demo1').style.color = 'blue';
+*/
+
+document.getElementById('demo').innerHTML = 5 + 6;
+document.write(5 + 6);
