3cd2f253b7c84ab5d8a6d56d3b97fe17d8713e23
xammpp <bagusdaffaf01@gmail.com>
2025-02-13 23:18:01 +0700

    ..

---
+++ php/php-string.php
@@ -0,0 +1,10 @@
+// string di php bisa menggunakan double quotes dan singel quotes
+
+// function strlen() berfungsi untuk menghitung panjang dari string 
+
+// function str_word_count() berfungsi untuk menghitung jumlah kata dari string 
+
+// function strtoupper() berfungsi untuk mengubah kata dari string menjadi huruf besar semuanya
+
+// function strtolower() berfungsi untuk mengubah kata dari string menjadi huruf kecil semuanya
+
